2f11e28dc6b22e60ff7f6b6bb25f843b9f8cef32
Fábio David Freitas <fabio.david.contato@gmail.com>
2020-11-12 16:17:41 -0300

    Created models and started views

--- games/suggestions/models.py
+++ games/suggestions/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Game(models.Model):
+    name = models.TextField()
+    app_id = models.IntegerField()
+    price = models.FloatField()
+    image = models.URLField(blank=True)
+    tags = models.CharField(max_length=1000)
+
+    
--- games/suggestions/views.py
+++ games/suggestions/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 # Create your views here.
 
 def index(request):
     return HttpResponse('<h1>Hi<h1>')
+
+def db(request):
+    response = requests.get('https://steamspy.com/api.php?request=all&page=0')
+    json = response.json()
+    #continuar quando a api voltarbuceta gcu infernooooooooooooooooooooooooooooo
+    pass
