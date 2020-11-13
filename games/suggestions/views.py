a491ccdfda16659efb23d828affe956dbd3541ab
Fábio David Freitas <fabio.david.contato@gmail.com>
2020-11-13 00:25:26 -0300

    Think I finished up with the constructor view

--- games/suggestions/views.py
+++ games/suggestions/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import requests
+import requests, json
+import os
+from models import Game
 
 # Create your views here.
 
@@ -8,7 +10,39 @@ def index(request):
     return HttpResponse('<h1>Hi<h1>')
 
 def db(request):
-    response = requests.get('https://steamspy.com/api.php?request=all&page=0')
-    json = response.json()
-    #continuar quando a api voltarbuceta gcu infernooooooooooooooooooooooooooooo
-    pass
+    f = open('suggestions/tops.txt', 'r')
+    string = f.read()
+
+    dic = json.loads(string)
+    lista = []
+    for app in dic:
+        lista.append(app)
+    
+    for app in lista:
+        result = requests.get(f'https://steamspy.com/api.php?request=appdetails&appid={app}')
+        json_obj = json.loads(result)
+        name = json_obj['name']
+        app_id = json_obj['appid']
+        price = json_obj['price']
+        image = f'https://steamcdn-a.akamaihd.net/steam/apps/{app_id}/header.jpg'
+
+        tags_obj = json_obj['tags']
+        tag_list = []
+        for tag in tags_obj:
+            tag_list.append(tag)
+        
+        #Converting tags list to a string so it can be saved as a CharField
+        separator = ', '
+        tags = separator.join(tag_list)
+
+        game = Game(
+            name=name,
+            app_id=app_id,
+            price=price,
+            image=image,
+            tags=tags
+        )
+        print(f'{name} Added to database!')
+        sleep(1) #Fucking gay ass pussy ass low computing power ass shitty ass API server
+  
+    return HttpResponse('Success')
