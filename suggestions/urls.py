fb1eafa5babe3332124f238f48606d9bdd3b711c
Fábio David Freitas <fabio.david.contato@gmail.com>
2020-12-22 21:23:32 -0300

    Added favicon

--- suggestions/urls.py
+++ suggestions/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('get-games/', views.getGames, name='get-games')
+    path('get-games/', views.getGames, name='get-games'),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url='/static/favicon.ico', permanent=True),
+    ),
 ]
